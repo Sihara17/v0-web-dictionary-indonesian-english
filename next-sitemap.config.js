78259391c32256245d6c6d1ecc3e8f7f2d73ecd3
Sihara17 <147967440+Sihara17@users.noreply.github.com>
2025-12-02 07:34:10 +0000

    Generate sitemap.xml with next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://v0-web-dictionary-indonesian-englis.vercel.app',
+  generateRobotsTxt: true, // otomatis bikin robots.txt
+  changefreq: 'daily',
+  priority: 0.7,
+};
